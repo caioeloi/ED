60110ae2257292e42a4b8bd08defebee02affcdc
caiozec <caioeloi09@gmail.com>
2022-05-05 19:18:37 -0300

    voltando do zero!

--- TP0/matdinamica/src/mat.c
+++ TP0/matdinamica/src/mat.c
@@ -1,172 +1,260 @@
 //---------------------------------------------------------------------
 // Arquivo	: mat.c
-// Conteudo	: implementacao do TAD MAT DINAMICAMENTE
+// Conteudo	: implementacao do TAD MAT 
 // Autor	: Wagner Meira Jr. (meira@dcc.ufmg.br)
-// Historico	: 2022-04-20 - arquivo criado
+// Historico	: 2021-10-18 - arquivo criado
+//		: 2021-10-21 - estrutura de diretorios
+//              : 2021-11-14 - adequacao para versao 1.1 memlog
 //---------------------------------------------------------------------
 
-#include "msgassert.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include "mat.h"
+#include "memlog.h"
+#include "msgassert.h"
 
-double **criaMatriz( int m, int n){
-    // verifica se os valores de m e n são validos
-    erroAssert(m>0,"Dimensao nula");
-    erroAssert(n>0,"Dimensao nula");
+// limite superior da inicializacao aleatoria
+#define INITRANDOMRANGE 10
+// Macro que realiza swap sem variavel auxiliar
+#define ELEMSWAP(x,y) (x+=y,y=x-y,x-=y)
 
-
-    double **M;
-    M = (double **)malloc(sizeof(double *) * m);
-    // verifica se a matriz foi alocada corretamente
-    erroAssert(M != NULL, "Erro ao alocar matriz!");
-
-    for (int i = 0; i < m; i++)
-    {
-        M[i] = (double *)malloc(sizeof(double)*n);
-        // verifica se a matriz foi alocada corretamente
-        erroAssert(M[i] != NULL, "Erro ao alocar matriz!");
-        
-
-    }
-    
-    return M;
-    
-}
-
-void zeraMatriz(double **M, int m, int n){
-
-    for (int i = 0; i < m; i++)
-    {
-        // adicionando 0 aos elementos da matriz por precaucao
-        for (int j = 0; i < n; j++)
-        {
-            M[i][j] = 0;
-        }
-    }
-}
-
-void leElemento(int *m, int *n){
-    FILE *arq;
-	arq = fopen("arquivo.txt", "r");
-    // verifica se foi possível abrir o arquivo
-    erroAssert(arq != NULL, "Erro ao ler arquivo");
-    int i,j = 0;
-    fscanf(arq, "%d %d", m, n);
-    fclose(arq);
-}
-
-void preencheMatriz(double **M, int m, int n){
-    FILE *arq;
-	arq = fopen("arquivo.txt", "r");
-    // verifica se foi possível abrir o arquivo
-    erroAssert(arq != NULL, "Erro ao ler arquivo");
-    int i,j = 0;
-    fscanf(arq, "%d %d", &m, &n);
-    printf("%d %d\n", m,n);
-    while (!feof(arq)){
-        for ( i = 0; i < m; i++)
-        {
-            for ( j = 0; j < n; j++)
-            {
-                fscanf(arq, "%lf ", &M[i][j]);
-            } 
-        }
-    }
-
-    fclose(arq);
-}
-
-double **somaMatriz(double **A, double **B, int ma, int na, int mb, int nb){
-    //verifica se as dimensões das matrizes são corretas para realizar a adição
-    erroAssert(ma == mb && na == nb, "Impossível realizar a adição");
-    double **C;
-    C = criaMatriz(ma,na);
-    for (int i = 0; i < ma; i++)
-    {
-        for (int j = 0; j < na; j++)
-        {
-            C[i][j] = A[i][j] + B[i][j];
-        }
-        
-    }
-    return C;
-}
-
-double **multiplicaMatriz(double **A, double **B, int ma, int na, int mb, int nb){
-
-    //verifica se as dimensões das matrizes são corretas para realizar a multiplicação
-    erroAssert(na == mb, "Impossível realizar a multiplicação");
-    double **C;
-    int i,j,k = 0;
-    C = criaMatriz(ma,nb);
-    for (i = 0; i < ma; i++){
-        for (j = 0; j < nb; j++){
-            for (k=0; k < na; k++){
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-    printf("\n");
-    return C;
-}
-
-
-double **transpoeMatriz(double **A, int m, int n){
-    double **B;
-    B = criaMatriz(n,m);
-    int i, j;
-
-    for (i = 0; i < n; i++){
-        for(j = 0 ; j < m; j++){
-            B[i][j] = A[j][i];
-        }
-    }
-
-    return B;
-}
-
-
-void imprimeMatriz(double **M, int m, int n){
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%lf ", M[i][j]);
-        }
-        printf("\n");
-        
-    }
-    
-}
-
-void liberaMatriz(double **M, int m){
-    for (int i = 0; i < m; i++)
-    {
-        free(M[i]);
-    }
-    free(M);
-}
-
-
-
-int main(int argc, char const *argv[])
+void criaMatriz(mat_tipo * mat, int tx, int ty, int id)
+// Descricao: cria matriz com dimensoes tx X ty
+// Entrada: mat, tx, ty, id
+// Saida: mat
 {
-    int m, n;
-
-    double **M;
-    leElemento(&m, &n);
-    M = criaMatriz(m, n);
-    preencheMatriz(M, m, n);
-    imprimeMatriz(M, m, n);
-    printf("\n");
-    printf("\n");
-    double **aux;
-    aux = transpoeMatriz(M, m, n);
-    double **aux2;
-    aux2 = somaMatriz(M, M, m,n,m,n);
+  mat = (mat_tipo *) malloc(sizeof(mat_tipo));
     
-    imprimeMatriz(aux, n, m);
+    erroAssert(mat != NULL, "Erro ao alocar matriz!");
 
-    return 0;
+    // verifica se os valores de tx e ty são validos
+    erroAssert(tx>0,"Dimensao nula");
+    erroAssert(ty>0,"Dimensao nula");
+    
+    mat->m = (double **)malloc(sizeof(double *) * tx);
+    // verifica se a matriz foi alocada corretamente
+    erroAssert(mat->m != NULL, "Erro ao alocar matriz!");
+    
+    for (int i = 0; i < tx; i++)
+    {
+        mat->m[i] = (double *)malloc(sizeof(double)*ty);
+        // verifica se a matriz foi alocada corretamente
+        erroAssert(mat->m[i] != NULL, "Erro ao alocar matriz!");
+
+    }
+    // inicializa as dimensoes da matriz
+    mat->tamx = tx;
+    mat->tamy = ty;
+    // inicializa o identificador da matriz, para rastreamento
+    mat->id = id;
+}
+
+void inicializaMatrizNula(mat_tipo * mat)
+// Descricao: inicializa mat com valores nulos 
+// Entrada: mat
+// Saida: mat
+{
+  int i, j;
+  // inicializa todos os elementos da matriz com 0, por seguranca 
+  for (i=0; i<mat->tamx; i++){
+    for(j=0; j<mat->tamy; j++){
+      mat->m[i][j] = 0;
+      ESCREVEMEMLOG((long int)(&(mat->m[i][j])),sizeof(double),mat->id);
+    }
+  }
+}
+
+void inicializaMatrizAleatoria(mat_tipo * mat)
+// Descricao: inicializa mat com valores aleatorios
+// Entrada: mat 
+// Saida: mat
+{
+  int i, j;
+  // inicializa a matriz com valores nulos, por seguranca
+  inicializaMatrizNula(mat);
+  // inicializa a parte alocada da matriz com valores aleatorios
+  for (i=0; i<mat->tamx; i++){
+    for(j=0; j<mat->tamy; j++){
+      mat->m[i][j] = drand48()*INITRANDOMRANGE;
+      ESCREVEMEMLOG((long int)(&(mat->m[i][j])),sizeof(double),mat->id);
+    }
+  }
+}
+
+double acessaMatriz(mat_tipo * mat)
+// Descricao: acessa mat para fins de registro de acesso 
+// Entrada: mat 
+// Saida: mat
+{
+  int i, j;
+  double aux, s=0.0;
+  for (i=0; i<mat->tamx; i++){
+    for(j=0; j<mat->tamy; j++){
+      aux = mat->m[i][j];
+      s+=aux;
+      LEMEMLOG((long int)(&(mat->m[i][j])),sizeof(double),mat->id);
+    }
+  }
+  return s; // apenas para evitar que acesso seja eliminado
+}
+
+void imprimeMatriz(mat_tipo * mat)
+// Descricao: imprime a matriz com a identificacao de linhas e colunas
+// Entrada: mat
+// Saida: impressao na saida padrao (stdout) 
+{
+  int i,j;
+
+  // imprime os identificadores de coluna
+  printf("%9s"," ");
+  for(j=0; j<mat->tamy; j++)
+    printf("%8d ",j);
+  printf("\n");
+
+  // imprime as linhas
+  for (i=0; i<mat->tamx; i++){
+    printf("%8d ",i);
+    for(j=0; j<mat->tamy; j++){
+      printf("%8.2f ",mat->m[i][j]);
+      LEMEMLOG((long int)(&(mat->m[i][j])),sizeof(double),mat->id);
+    }
+    printf("\n");
+  }
+}
+
+void escreveElemento(mat_tipo * mat, int x, int y, double v)
+// Descricao: atribui o valor v ao elemento (x,y) de mat
+// Entrada: mat, x, y, v
+// Saida: mat
+{
+  // verifica se x e y sao validos
+  erroAssert((x<0)||(x>=mat->tamx),"Indice invalido");
+  erroAssert((y<0)||(y>=mat->tamy),"Indice invalido");
+
+  mat->m[x][y] = v;
+  ESCREVEMEMLOG((long int)(&(mat->m[x][y])),sizeof(double),mat->id);
+}
+
+double leElemento (mat_tipo * mat, int x, int y)
+// Descricao: retorna o elemento (x,y) de mat 
+// Entrada: mat, x, y
+// Saida: mat[x][y] 
+{
+  // verifica se x e y sao validos
+  erroAssert((x<0)||(x>=mat->tamx),"Indice invalido");
+  erroAssert((y<0)||(y>=mat->tamy),"Indice invalido");
+
+  LEMEMLOG((long int)(&(mat->m[x][y])),sizeof(double),mat->id);
+  return mat->m[x][y];
+}
+
+void copiaMatriz(mat_tipo *src, mat_tipo * dst, int dst_id)
+// Descricao: faz uma copia de src em dst
+// Entrada: src, dst_id
+// Saida: dst
+{
+  int i,j;
+
+  // cria novamente a matriz dst para ajustar as suas dimensoes
+  criaMatriz(dst,src->tamx, src->tamy,dst_id);
+  // inicializa a matriz dst como nula
+  inicializaMatrizNula(dst);
+  // copia os elementos da matriz src
+  for (i=0; i<src->tamx; i++){
+    for(j=0; j<src->tamy; j++){
+      dst->m[i][j] = src->m[i][j];
+      LEMEMLOG((long int)(&(src->m[i][j])),sizeof(double),src->id);
+      ESCREVEMEMLOG((long int)(&(dst->m[i][j])),sizeof(double),dst->id);
+    }
+  }
+}
+
+void somaMatrizes(mat_tipo *a, mat_tipo *b, mat_tipo *c)
+// Descricao: soma as matrizes a e b e armazena o resultado em c
+// Entrada: a, b
+// Saida: c
+{
+  int i,j;
+  // verifica se as dimensoes das matrizes a e b sao as mesmas
+  erroAssert(a->tamx==b->tamx,"Dimensoes incompativeis");
+  erroAssert(a->tamy==b->tamy,"Dimensoes incompativeis");
+
+  // inicializa a matriz c garantindo a compatibilidade das dimensoes
+  criaMatriz(c,a->tamx, a->tamy, c->id);
+  inicializaMatrizNula(c);
+
+  // faz a soma elemento a elemento
+  for (i=0; i<a->tamx; i++){
+    for(j=0; j<a->tamy; j++){
+      c->m[i][j] = a->m[i][j]+b->m[i][j];
+      LEMEMLOG((long int)(&(a->m[i][j])),sizeof(double),a->id);
+      LEMEMLOG((long int)(&(b->m[i][j])),sizeof(double),b->id);
+      ESCREVEMEMLOG((long int)(&(c->m[i][j])),sizeof(double),c->id);
+    }
+  }
+}
+
+void multiplicaMatrizes(mat_tipo *a, mat_tipo *b, mat_tipo *c)
+// Descricao: multiplica as matrizes a e b e armazena o resultado em c
+// Entrada: a,b
+// Saida: c
+{
+  int i,j,k;
+  // verifica a compatibilidade das dimensoes 
+  erroAssert(a->tamy==b->tamx,"Dimensoes incompativeis");
+
+  // cria e inicializa a matriz c
+  criaMatriz(c,a->tamx, b->tamy,c->id);
+  inicializaMatrizNula(c);
+
+  // realiza a multiplicacao de matrizes
+  for (i=0; i<c->tamx;i++){
+    for (j=0; j<c->tamy;j++){
+      for (k=0; k<a->tamy;k++){
+        c->m[i][j] += a->m[i][k]*b->m[k][j];
+        LEMEMLOG((long int)(&(a->m[i][k])),sizeof(double),a->id);
+        LEMEMLOG((long int)(&(b->m[k][j])),sizeof(double),b->id);
+        ESCREVEMEMLOG((long int)(&(c->m[i][j])),sizeof(double),c->id);
+      }
+    }
+  }
+}
+
+void transpoeMatriz(mat_tipo *a)
+// Descricao: transpoe a matriz a
+// Entrada: a
+// Saida: a
+{
+  int i,j,dim;
+  
+  // determina a maior dimensao entre tamx e tamy
+  dim = (a->tamx>a->tamy?a->tamx:a->tamy);
+
+  // faz a transposicao como se a matriz fosse quadrada
+  for (i=0; i<dim; i++){
+    for(j=i+1; j<dim; j++){
+      ELEMSWAP((a->m[i][j]),(a->m[j][i]));
+      ESCREVEMEMLOG((long int)(&(a->m[i][j])),sizeof(double),a->id);
+      ESCREVEMEMLOG((long int)(&(a->m[j][i])),sizeof(double),a->id);
+    }
+  }
+  // inverte as dimensoes da matriz transposta
+  ELEMSWAP(a->tamx,a->tamy);
+}
+
+void destroiMatriz(mat_tipo *a)
+// Descricao: destroi a matriz a, que se torna inacessível
+// Entrada: a
+// Saida: a
+{
+  // apenas um aviso se a matriz for destruida mais de uma vez
+  avisoAssert(((a->tamx>0)&&(a->tamy>0)),"Matriz já foi destruida");
+    
+    for (int i = 0; i < a->tamx; i++)
+    {
+        free(a->m[i]);
+    }
+    free(a->m);
+    free(a);
 }
